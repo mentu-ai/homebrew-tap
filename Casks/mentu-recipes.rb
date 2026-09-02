fe436a25969ec3df20941ca7620a41ec9ee40700
Rashid Azarang <rashid.azarang.eg@gmail.com>
2026-09-02 10:07:32 -0600

    feat: bump mentu-recipes-bin to attested v0.2.2; cask to the notarized v0.2.2 package

--- Casks/mentu-recipes.rb
+++ Casks/mentu-recipes.rb
@@ -1,6 +1,6 @@
 cask "mentu-recipes" do
-  version "0.1.0"
-  sha256 "a6ab0e0125c90cb1d57361972dc9eeada229a7d9b4c8d3599388c1ada6cee560"
+  version "0.2.2"
+  sha256 "8c644b5192f9466e4b414e69e37a4ee33dff55cb723c9f95f7ccd8bc1bb5101a"
 
   url "https://github.com/mentu-ai/mentu-recipes/releases/download/v#{version}/mentu-recipes-#{version}-macos-arm64.pkg"
   name "Mentu Recipes"
--- Formula/mentu-recipes-bin.rb
+++ Formula/mentu-recipes-bin.rb
@@ -1,18 +1,18 @@
 class MentuRecipesBin < Formula
   desc "Source-available runner for file-based agent workflows"
   homepage "https://github.com/mentu-ai/mentu-recipes"
-  version "0.2.1"
+  version "0.2.2"
   license :cannot_represent
 
   on_macos do
     on_arm do
       url "https://github.com/mentu-ai/mentu-recipes/releases/download/v#{version}/mentu-recipes-macos-arm64"
-      sha256 "02f9b1f051c7a399a5dca7c8f940e1a59680a066b07afcf2b1ecb1b3bd924b24"
+      sha256 "dda5297933686788e9b94c22fc43824c5c22a849af77f6e3109c686407466458"
     end
 
     on_intel do
       url "https://github.com/mentu-ai/mentu-recipes/releases/download/v#{version}/mentu-recipes-macos-x86_64"
-      sha256 "057f5d5a59a5c3319fb740651121bbb255252d727347ab4d84896d4b4e562be8"
+      sha256 "dcb0d6d64c2a9795b84695282452571127abf352bc9f11bec3598b409612a714"
     end
   end
 
